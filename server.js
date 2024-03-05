3fd914b23e6515e357fa560a7ff8ae6b9334bb0a
Anoj Roshan <s223264153@deakin.edu.au>
2024-03-05 20:14:39 +1100

    first commit

---
+++ server.js
@@ -0,0 +1,22 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const PORT = process.env.PORT || 3000; // Set the port
+
+const server = http.createServer((req, res) => {
+    // Read the HTML file
+    fs.readFile(path.join(__dirname, 'index.html'), (err, data) => {
+        if (err) {
+            res.writeHead(500, {'Content-Type': 'text/plain'});
+            res.end('Internal Server Error');
+        } else {
+            res.writeHead(200, {'Content-Type': 'text/html'});
+            res.end(data);
+        }
+    });
+});
+
+server.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
